f08c096c2c250c3955867669d34399929df5f4c4
Marco Antonio Tancredo <marcotancredo@gmail.com>
2024-08-13 21:30:24 -0300

    Validação customizada email não repetido na inserção de Cliente

--- src/main/java/com/marcotancredo/cursomc/domain/Cliente.java
+++ src/main/java/com/marcotancredo/cursomc/domain/Cliente.java
@@ -20,7 +20,7 @@ public class Cliente implements Serializable {
     @Column(name = "nome", nullable = false)
     private String nome;
 
-    @Column(name = "email", nullable = false)
+    @Column(name = "email", nullable = false/*, unique = true*/)
     private String email;
 
     @Column(name = "cpf_cnpj")
--- src/main/java/com/marcotancredo/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/marcotancredo/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,16 +1,22 @@
 package com.marcotancredo.cursomc.services.validation;
 
+import com.marcotancredo.cursomc.domain.Cliente;
 import com.marcotancredo.cursomc.domain.enums.TipoCliente;
 import com.marcotancredo.cursomc.dto.ClienteNewDTO;
+import com.marcotancredo.cursomc.repositories.ClienteRepository;
 import com.marcotancredo.cursomc.resources.exceptions.FieldMessage;
 import com.marcotancredo.cursomc.services.validation.utils.BR;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+    @Autowired
+    private ClienteRepository repository;
 
     @Override
     public void initialize(ClienteInsert amm) {
@@ -26,6 +32,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
         if (TipoCliente.PESSOA_JURIDICA.getCod() == objDto.getTipo() && !BR.isValidCPF(objDto.getCpfCnpj())) {
             list.add(new FieldMessage("cpfCnpj","CNPJ Inválido"));
+        }
+
+        Cliente aux = repository.findByEmail(objDto.getEmail());
+
+        if (aux != null) {
+            list.add(new FieldMessage("email", "Email já existente"));
         }
 
         for (FieldMessage e : list) {
